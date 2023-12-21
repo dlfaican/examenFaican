6257ac3d50956ca9680bbc0d55160530f4021f7c
DomenicaF23 <dlfaican@espe.edu.ec>
2023-12-20 21:12:14 -0500

    commit final

--- src/main/java/ec/edu/espe/examenfaican/examen/dao/AsignaturaRepository.java
+++ src/main/java/ec/edu/espe/examenfaican/examen/dao/AsignaturaRepository.java
@@ -1,13 +1,14 @@
 package ec.edu.espe.examenfaican.examen.dao;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-import org.hibernate.mapping.List;
 import org.springframework.data.repository.CrudRepository;
 
+import ec.edu.espe.examenfaican.examen.domain.AlumnoAsignatura;
 import ec.edu.espe.examenfaican.examen.domain.Asignatura;
 
 public interface AsignaturaRepository extends CrudRepository<Asignatura, String> {
-    //List<Asignatura>findByNombreAndCreditos(String nombre, BigDecimal creditos);
+    List<AlumnoAsignatura> findByCodAlumno(Integer codAlumno);
 }
 
